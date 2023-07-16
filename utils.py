b71ded4ad800db5eedbf2225724b19fc7e74088c
trevorwitter <trevorgwitter@gmail.com>
2023-07-16 09:21:03 -0400

    update eval

--- utils.py
+++ utils.py
@@ -10,6 +10,8 @@ from torchvision.io import read_image
 import torch.nn as nn
 import torch.nn.functional as F
 
+import matplotlib.pyplot as plt
+
 
 def multi_acc(y_pred, y_test):
     y_pred_softmax = torch.log_softmax(y_pred, dim = 1)
@@ -21,12 +23,13 @@ def multi_acc(y_pred, y_test):
 
 
 def accuracy_score(net, data_loader, gpu=False):
-    """Returns classifier accuracy score"""
     if gpu == False:
         device = torch.device("cpu")
     elif gpu == True:
         device = torch.device("mps")
     net = net.to(device)
+    correct_pred = {classname: 0 for classname in data_loader.dataset.classes}
+    total_pred = {classname: 0 for classname in data_loader.dataset.classes}
     correct = 0
     total = 0
     with torch.no_grad():
@@ -35,10 +38,31 @@ def accuracy_score(net, data_loader, gpu=False):
             inputs = inputs.to(device)
             labels = labels.to(device)
             outputs = net(inputs)
-            _, predicted = torch.max(outputs.data, 1)
+            _, predictions = torch.max(outputs, 1)
             total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    return correct/total
+            correct += (predictions == labels).sum().item()
+            for label, prediction in zip(labels, predictions):
+                if label == prediction:
+                    correct_pred[data_loader.dataset.classes[label]] += 1
+                total_pred[data_loader.dataset.classes[label]] += 1
+    acc = correct/total
+    class_acc = []
+    for classname, correct_count in correct_pred.items():
+        accuracy = 100 *float(correct_count) / total_pred[classname]
+        class_acc.append((classname, accuracy))
+    return acc, class_acc
+
+
+def plot_class_accuracy(class_acc):
+    class_acc = sorted(class_acc, key=lambda x: x[1])
+    classes = [x[0] for x in class_acc]
+    accuracy = [x[1] for x in class_acc]
+    fig, ax = plt.subplots(figsize=(10,int(len(classes)/5)))
+    ax.barh(classes, accuracy, align='center')
+    ax.set_xlabel("Accuracy %")
+    ax.set_title("Model Performance by Class")
+    plt.margins(0.005, 0.005)
+    plt.show()
 
 
 if __name__ == "__main__":
